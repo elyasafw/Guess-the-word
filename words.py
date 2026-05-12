a2881b9414aae4ed0542ccf0d7117a63e7af485a
elyasaf waisman <elyasafw1997@gmail.com>
2026-05-12 21:37:03 +0300

    Add words dictionary with 5 categories for hangman game

---
+++ words.py
@@ -0,0 +1,32 @@
+words = {
+    "animals": [
+        "elephant", "dolphin", "penguin", "leopard", "gorilla",
+        "crocodile", "flamingo", "hedgehog", "kangaroo", "platypus",
+        "wolverine", "cheetah", "hamster", "octopus", "tortoise",
+        "porcupine", "butterfly", "chameleon", "salamander", "armadillo"
+    ],
+    "countries": [
+        "brazil", "canada", "germany", "japan", "mexico",
+        "australia", "sweden", "nigeria", "argentina", "thailand",
+        "portugal", "ukraine", "indonesia", "colombia", "pakistan",
+        "malaysia", "ethiopia", "vietnam", "morocco", "norway"
+    ],
+    "food": [
+        "avocado", "broccoli", "cinnamon", "dumpling", "eggplant",
+        "falafel", "granola", "hummus", "jalapeno", "kimchi",
+        "lasagna", "mozzarella", "noodles", "omelet", "pancake",
+        "quinoa", "raspberry", "spaghetti", "tortilla", "waffle"
+    ],
+    "sports": [
+        "archery", "badminton", "baseball", "bowling", "cricket",
+        "cycling", "fencing", "football", "gymnastics", "handball",
+        "hockey", "judo", "kayaking", "lacrosse", "marathon",
+        "rowing", "skiing", "swimming", "volleyball", "wrestling"
+    ],
+    "technology": [
+        "algorithm", "bandwidth", "compiler", "database", "encryption",
+        "firewall", "gateway", "hardware", "interface", "javascript",
+        "keyboard", "laptop", "monitor", "network", "operating",
+        "processor", "quantum", "router", "software", "terminal"
+    ]
+}
